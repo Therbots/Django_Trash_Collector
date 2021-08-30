2fa4cb853ccb98a846a05a96ffa6e40700277e5a
unknown <mekinleydsaintvictor@outlook.com>
2021-08-30 12:18:47 -0400

    Fixed database, and corrected migrations.

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
 
 # TODO: Finish customer model by adding necessary properties to fulfill user stories
 class Customer(models.Model):
-    name = models.CharField(max_length=50),
-    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE),
-    address = models.CharField(max_length=50),
-    zip_code = models.CharField(max_length=50),
-    balance = models.IntegerField(max_length=50, default=0),
-    weekly_pickup_day = models.CharField(max_length=50),
-    one_time_pickup = models.DateField(null=True, blank=True),
-    suspend_start = models.DateField(null=True, blank=True),
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    address = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=50)
+    balance = models.IntegerField(default=0)
+    weekly_pickup_day = models.CharField(max_length=50)
+    one_time_pickup = models.DateField(null=True, blank=True)
+    suspend_start = models.DateField(null=True, blank=True)
     suspend_end = models.DateField(null=True, blank=True)
 
     def __str__(self):
