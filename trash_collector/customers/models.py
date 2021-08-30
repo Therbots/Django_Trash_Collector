b822ab57cddd3df18821b770c326bdf8603a198e
unknown <mekinleydsaintvictor@outlook.com>
2021-08-30 10:43:54 -0400

    Added customer and employee models.

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -6,4 +6,14 @@ from django.db import models
 
 class Customer(models.Model):
     name = models.CharField(max_length=50)
-    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE),
+    address = models.CharField(max_length=50),
+    zip_code = models.CharField(max_length=50),
+    balance = models.IntegerField(max_length=50),
+    weekly_pickup_day = models.CharField(max_length=50),
+    one_time_pickup = models.DateField(null=True),
+    suspend_start = models.DateField(null=True),
+    suspend_end = models.DateField(null=True)
+
+    def __str__(self):
+        return self.name
