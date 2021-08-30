2c527c3d7e7458dce8fb8003f59917c7ddfc4c0f
unknown <mekinleydsaintvictor@outlook.com>
2021-08-30 12:12:18 -0400

    Added migrations.

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -8,11 +8,11 @@ class Customer(models.Model):
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE),
     address = models.CharField(max_length=50),
     zip_code = models.CharField(max_length=50),
-    balance = models.IntegerField(max_length=50),
+    balance = models.IntegerField(max_length=50, default=0),
     weekly_pickup_day = models.CharField(max_length=50),
-    one_time_pickup = models.DateField(null=True),
-    suspend_start = models.DateField(null=True),
-    suspend_end = models.DateField(null=True)
+    one_time_pickup = models.DateField(null=True, blank=True),
+    suspend_start = models.DateField(null=True, blank=True),
+    suspend_end = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.name
