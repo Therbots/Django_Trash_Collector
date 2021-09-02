c097b0da2a783587512f38dcc9c8e6d58f4108d0
taylor herbots <therbots@live.com>
2021-09-01 20:39:48 -0500

    created confirm.html and added path in urls.py. created confirm function

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -8,5 +8,6 @@ from . import views
 app_name = "employees"
 urlpatterns = [
     path('', views.index, name="index"),
-    path('create/', views.create, name="create")
+    path('create/', views.create, name="create"),
+    path('confirm/<int:user_id>', views.confirm, name="confirm")
 ]
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -13,23 +13,23 @@ import calendar
 
 
 def index(request):
-
+    user = request.user
     try:
         # This line inside the 'try' will return the customer record of the logged-in user if one exists
         logged_in_employee = Employee.objects.get(user=user)
+        employee_zip = logged_in_employee.zip_code
+        today = date.today()
+        str_day = (calendar.day_name[today.weekday()])
+
+        Customer = apps.get_model('customers.Customer')
+        all_customers = Customer.objects.filter(zip_code=employee_zip, weekly_pickup_day=str_day) | Customer.objects.filter(zip_code=employee_zip, one_time_pickup=today) and Customer.objects.filter(zip_code=employee_zip, suspend_start__gte=today, suspend_end__lte=today) | Customer.objects.filter(zip_code=employee_zip, suspend_start__isnull=True, suspend_end__isnull=True)
+        return render(request, 'employees/index.html', {'all_customers' : all_customers})
+
     except:
         return render(request, 'employees/create.html')
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-    user = request.user
-    single_user = Employee.objects.get(user=user)
-    employee_zip = single_user.zip_code
-    today = date.today()
-    str_day = (calendar.day_name[today.weekday()])
-
-    Customer = apps.get_model('customers.Customer')
-    all_customers = Customer.objects.filter(zip_code=employee_zip, weekly_pickup_day=str_day) | Customer.objects.filter(zip_code=employee_zip, one_time_pickup=today) and Customer.objects.filter(zip_code=employee_zip, suspend_start__gte=today, suspend_end__lte=today) | Customer.objects.filter(zip_code=employee_zip, suspend_start__isnull=True, suspend_end__isnull=True)
-    return render(request, 'employees/index.html', {'all_customers' : all_customers})
-
+    
+    
 
 
 def create(request):
@@ -42,4 +42,18 @@ def create(request):
         return HttpResponseRedirect(reverse('employees:index'))
     else:
         return render(request, 'employees/create.html')
+
+def confirm(request, user_id):
+    Customer = apps.get_model('customers.Customer')
+    customer_pickup = Customer.objects.get(pk=user_id)
+    context = {
+        'customer_pickup': customer_pickup
+    }
+    if request.method == "POST":
+        customer_pickup.balance += 2
+        customer_pickup.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        return render(request, 'employees/confirm.html', context)
+
 
